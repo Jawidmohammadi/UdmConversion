a746b4c67fb03f7245622897f9b280305a91e8bd
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-04-29 01:00:27 -0600

    added a new class

--- src/Method.java
+++ src/Method.java
@@ -19,19 +19,40 @@ public class Method {
 //      int finalScore = score + (levelCompleted * bonus);
 //      System.out.println("Second Final Score is: " + finalScore);
 //    }
+//
+//    System.out.println("This is the method Call:  " + calculateScore(false, 100, 2, 100));
+//    System.out.println("This is the Second method Call:  " + calculateScore(true, 200, 4, 200));
+//  }
+//
+//  public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+//    int finalScore;
+//    if (gameOver){
+//    finalScore = score + (levelCompleted * bonus);
+//    return finalScore;
+//    } else {
+//      return -1;
+//    }
+    int highScorePosition = calculateHighScorePosition(1000);
+    displayHighScorePosition("John", highScorePosition);
 
-    System.out.println("This is the method Call:  " + calculateScore(true, 100, 2, 100));
-    System.out.println("This is the Second method Call:  " + calculateScore(true, 200, 4, 200));
+    highScorePosition = calculateHighScorePosition(500);
+    displayHighScorePosition("Jawid", highScorePosition);
   }
 
-  public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
-    int finalScore;
-    if (gameOver){
-    finalScore = score + (levelCompleted * bonus);
-    } else {
-      return -1;
-    }
-    return finalScore;
+
+  public static void displayHighScorePosition(String playerName, int highScorePosition){
+    System.out.println(playerName + "  managed to get into highScorePosition " + highScorePosition);
   }
 
+  public static int calculateHighScorePosition(int playerScore){
+    int position = 4;
+    if (playerScore >= 1000)
+      position = 1;
+    else if (playerScore >= 500)
+      position = 2;
+    else if (playerScore >= 100)
+      position = 3;
+      return position;
+  }
 }
+

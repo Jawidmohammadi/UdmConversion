b44382ffdae30f5231d3a08c59a9b9d1077cf9e7
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-04-29 02:06:32 -0600

    speedconverter

---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,40 @@
+public class PositiveNegativeZero {
+
+  public static void main(String[] args) {
+    checkNumber(10);
+    checkNumber(0);
+    checkNumber(-5);
+
+    printConversion(120);
+
+
+  }
+
+  public static void checkNumber(int number){
+    if (number > 0) {
+      System.out.println("Positive");
+    }
+    else if (number < 0) {
+      System.out.println("Negative");
+    } else {
+      System.out.println("Zero");
+    }
+  }
+
+  public static long toMilePerHour(double kiloMeter){
+    if (kiloMeter < 0){
+      return -1;
+    }
+    else{
+      return (long) (kiloMeter * 0.621371);
+    }
+  }
+
+  public static void printConversion(double kiloMeter){
+    if (kiloMeter < 0) {
+      System.out.println("invalid value");
+    } else {
+      System.out.println(kiloMeter + " km/h" + " = " + toMilePerHour(kiloMeter) + " mi/h");
+    }
+  }
+}

678758a59056e8e95086339a56a9ada303043e59
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-05-05 02:11:07 -0600

    PalindromeNumber

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,14 +1,20 @@
 public class PalindromeNumber {
 
   public static void main(String[] args) {
-
+    System.out.println(isPalindrome(45054));
+    System.out.println(isPalindrome(102));
+    System.out.println(isPalindrome(456979654));
   }
 
   public static boolean isPalindrome(int number) {
+    int reversed = 0;
+    int lastDigit = 0;
+    int original = number;
     while (number > 0) {
-      int reverse = number % 10;
-      int reversed = reverse++;
+      lastDigit = number % 10;
+      reversed = reversed * 10 + lastDigit;
+      number /= 10;
     }
-
+    return original == reversed;
   }
 }

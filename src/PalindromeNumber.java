d7f0ddc1e1fe9fb2a985b65a94c597b77fdcf63c
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-05-04 19:02:11 -0600

    PalindromeNumber

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,14 @@
+public class PalindromeNumber {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static boolean isPalindrome(int number) {
+    while (number > 0) {
+      int reverse = number % 10;
+      int reversed = reverse++;
+    }
+
+  }
+}
